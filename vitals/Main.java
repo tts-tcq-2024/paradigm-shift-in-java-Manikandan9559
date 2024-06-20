fa6f264d8b6308d4767c3ea9ed68152e5927c901
Manikandan9559 <manibcse@gmail.com>
2024-06-20 15:52:55 +0530

    Update Main.java

--- vitals/Main.java
+++ vitals/Main.java
@@ -2,23 +2,39 @@ package vitals;
 
 public class Main {
     static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
-    	if(checkTemperature(temperature)) {
-    		return checkSoc(soc, chargeRate);
+    	if(checkMinTemperature(temperature)) {
+    		return checkMaxTemperature(temperature, soc, chargeRate);
     	} else {
     		return false;
     	}
     }
     
-    private static boolean checkTemperature(float temperature) {
-    	if(temperature < 0 || temperature > 45) {
+    private static boolean checkMinTemperature(float temperature) {
+    	if(temperature < 0) {
             System.out.println("Temperature is out of range!");
             return false;
         }
     	return true;
     }
     
-    private static boolean checkSoc(float soc, float chargeRate) {
-    	if(soc < 20 || soc > 80) {
+    private static boolean checkMaxTemperature(float temperature, float soc, float chargeRate) {
+    	if(temperature > 45) {
+            System.out.println("Temperature is out of range!");
+            return false;
+        }
+    	return checkMinSoc(soc, chargeRate);
+    }
+    
+    private static boolean checkMinSoc(float soc, float chargeRate) {
+    	if(soc < 20) {
+    		System.out.println("State of Charge is out of range!");
+    		return false;
+    	}
+    	return checkMaxSoc(soc, chargeRate);
+    }
+    
+    private static boolean checkMaxSoc(float soc, float chargeRate) {
+    	if(soc > 80) {
     		System.out.println("State of Charge is out of range!");
     		return false;
     	}
