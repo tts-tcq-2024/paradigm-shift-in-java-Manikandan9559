02c678bc57ee6d39f198f0776f231d22f9845af4
Manikandan9559 <manibcse@gmail.com>
2024-06-26 14:03:07 +0530

    Update Main.java

--- vitals/Main.java
+++ vitals/Main.java
@@ -11,7 +11,7 @@ public class Main {
     
     private static boolean checkMinTemperature(float temperature) {
     	if(temperature < 0) {
-            System.out.println("Temperature is out of range!");
+    		printStatus("Temperature is out of range!");
             return false;
         }
     	return true;
@@ -19,7 +19,7 @@ public class Main {
     
     private static boolean checkMaxTemperature(float temperature, float soc, float chargeRate) {
     	if(temperature > 45) {
-            System.out.println("Temperature is out of range!");
+    		printStatus("Temperature is out of range!");
             return false;
         }
     	return checkMinSoc(soc, chargeRate);
@@ -27,7 +27,7 @@ public class Main {
     
     private static boolean checkMinSoc(float soc, float chargeRate) {
     	if(soc < 20) {
-    		System.out.println("State of Charge is out of range!");
+    		printStatus("State of Charge is out of range!");
     		return false;
     	}
     	return checkMaxSoc(soc, chargeRate);
@@ -35,7 +35,7 @@ public class Main {
     
     private static boolean checkMaxSoc(float soc, float chargeRate) {
     	if(soc > 80) {
-    		System.out.println("State of Charge is out of range!");
+    		printStatus("State of Charge is out of range!");
     		return false;
     	}
     	return checkChargeRate(chargeRate);
@@ -43,15 +43,19 @@ public class Main {
     
     private static boolean checkChargeRate(float chargeRate) {
     	if(chargeRate > 0.8) {
-            System.out.println("Charge Rate is out of range!");
+    		printStatus("Charge Rate is out of range!");
             return false;
         }
     	return true;
     }
+    
+    private static void printStatus(String status) {
+    	 System.out.println(status);
+    }
 
     public static void main(String[] args) {
-        assert(batteryIsOk(25, 70, 0.7f) == true);
-        assert(batteryIsOk(50, 85, 0.0f) == false);
-        System.out.println("Some more tests needed");
+        batteryIsOk(25, 70, 0.7f);
+        batteryIsOk(50, 85, 0.0f);
+        printStatus("Some more tests needed");
     }
 }
