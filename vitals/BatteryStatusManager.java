171eb6bd2f48d8940f0ac50603af2727a9f65f07
Manikandan9559 <manibcse@gmail.com>
2024-07-25 16:37:01 +0530

    Update and rename Main.java to BatteryStatusManager.java

---
+++ vitals/BatteryStatusManager.java
@@ -0,0 +1,49 @@
+package vitals;
+
+public class BatteryStatusManager {
+	private static final boolean IS_TEMPERATURE_WARNING = true;
+	private static final boolean IS_SOC_WARNING = true;
+	private static final boolean IS_CHARGE_RATE_WARNING = true;
+	static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
+		boolean checkTemperatureFlag = checkTemperature(temperature);
+		boolean checkSocFlag = checkSoc(soc);
+		boolean checkChargeRate = checkChargeRate(chargeRate);
+		return checkTemperatureFlag && checkSocFlag && checkChargeRate;
+	}
+
+	private static boolean checkTemperature(float temperature) {
+		TemperatureStatus temperatureStatus = new TemperatureStatus();
+		String temperatureValue = temperatureStatus.getTemperatureStatus(temperature);
+		if((temperatureValue.endsWith("WARNING") && IS_TEMPERATURE_WARNING) || (temperatureValue.endsWith("BREACH"))) {
+			BatteryStatusPrinter.printStatus(temperatureValue);
+			return false;
+		}
+		return true;
+	}
+
+	private static boolean checkSoc(float soc) {
+		SocStatus socStatus = new SocStatus();
+		String socValue = socStatus.getSocStatus(soc);
+		if((socValue.endsWith("WARNING") && IS_SOC_WARNING) || (socValue.endsWith("BREACH"))) {
+			BatteryStatusPrinter.printStatus(socValue);
+			return false;
+		}
+		return true;
+	}
+
+	private static boolean checkChargeRate(float chargeRate) {
+		ChargeRateStatus chargeRateStatus = new ChargeRateStatus();
+		String chargeRateValue = chargeRateStatus.getChargeRateStatus(chargeRate);
+		if((chargeRateValue.endsWith("WARNING") && IS_CHARGE_RATE_WARNING) || (chargeRateValue.endsWith("BREACH"))) {
+			BatteryStatusPrinter.printStatus(chargeRateValue);
+			return false;
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		batteryIsOk(25, 70, 0.9f);
+		batteryIsOk(50, 85, 0.78f);
+		System.out.println("Some more tests needed");
+	}
+}
